64d2ca921f3665c643e618013b7b87723f61d06e
Chandan Kalia <ckkalai15@gmail.com>
2024-08-08 19:47:04 +0530

    first commit

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -1,76 +1,35 @@
+import { useEffect, useState } from "react"
 import PreviousSearches from "../components/PreviousSearches"
 import RecipeCard from "../components/RecipeCard"
 
+
 export default function Recipes(){
-    const recipes = [
-        {
-            title: "Chicken Pan Pizza",
-            image: "/img/gallery/img_1.jpg",
-            authorImg: "/img/top-chiefs/img_1.jpg",
-        }, 
-        {
-            title: "Roasted Tomato Soup",
-            image: "/img/gallery/img_2.jpg",
-            authorImg: "/img/top-chiefs/img_2.jpg",
-        },
-        {
-            title: "Grilled Sandwich",
-            image: "/img/gallery/img_3.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-        },
-        {
-            title: "Rava Upma",
-            image: "/img/gallery/img_4.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-        },
-        {
-            title: "Japanese Sushi",
-            image: "/img/gallery/img_10.jpg",
-            authorImg: "/img/top-chiefs/img_6.jpg",
-        },
-        {
-            title: "Chicken Burger",
-            image: "/img/gallery/img_5.jpg",
-            authorImg: "/img/top-chiefs/img_4.jpg",
-        }, 
-        {
-            title: "Spaghetti and Meatballs",
-            image: "/img/gallery/img_6.jpg",
-            authorImg: "/img/top-chiefs/img_2.jpg",
-        },
-        {
-            title: "Tandoori Chicken",
-            image: "/img/gallery/img_7.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-        },
-        {
-            title: "Paldo Korean Buffet",
-            image: "/img/gallery/img_8.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
-        },
-        {
-            title: "Grilled Flank Steak",
-            image: "/img/gallery/img_9.jpg",
-            authorImg: "/img/top-chiefs/img_6.jpg",
-        },
-        {
-            title: "Berry Tarts",
-            image: "/img/gallery/img_11.jpg",
-            authorImg: "/img/top-chiefs/img_3.jpg",
-        },
-        {
-            title: "Cucumber Salad",
-            image: "/img/gallery/img_12.jpg",
-            authorImg: "/img/top-chiefs/img_5.jpg",
+    const [meals,setmeals] = useState([])
+const [loader,setloader]=useState(true)
+    const fetchMeals = async()=>{
+        let tempMeal = []
+        for(let i=0;i<12;i++){
+            const response = await fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            const json = await response.json()
+            tempMeal.push(json.meals[0])
+            
         }
-    ].sort(() => Math.random() - 0.5)
+        setmeals(tempMeal)
+setloader(false)
+    }
+
+    useEffect(()=>{
+        fetchMeals()
+    },[])
 
     return (
         <div>
-            <PreviousSearches />
+            <PreviousSearches setmeals={setmeals} />
             <div className="recipes-container">
-                {/* <RecipeCard /> */}
-                {recipes.map((recipe, index) => (
+
+                {loader && <span className="loader"></span>}
+            
+                { meals.length > 1 && meals.map((recipe, index) => (
                     <RecipeCard key={index} recipe={recipe} />
                 ))}
             </div>
